27ff33a382edf29df5d7f7bcfa53b77295548cdf
Antonio Davide <contact@antoniodavide.dev>
2023-04-01 18:50:24 +0200

    style(tuple_sequence.h): removed + 1, ordered if statements

--- C++/tuple_sequence.h
+++ C++/tuple_sequence.h
@@ -8,14 +8,14 @@ namespace impl {
 template<typename T, size_t Index>
 T get_sequence_value() {
     if constexpr(std::is_same_v<T, bool>) return true;
-    else if constexpr(std::is_integral_v<T>) return 42;
     else if constexpr(std::is_same_v<T, std::string>) return "thestring";
+    else if constexpr(std::is_integral_v<T>) return 42;
     ...
 }
 
 template<typename... Args, size_t... Sequence>
 auto tuple_sequence(std::index_sequence<Sequence...>) {
-    return std::make_tuple(impl::get_sequence_value<Args, Sequence + 1>()...);
+    return std::make_tuple(impl::get_sequence_value<Args, Sequence>()...);
 }
 
 } // namespace impl
