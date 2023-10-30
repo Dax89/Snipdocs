60155a1fd9d016e14c791e00a635198289af6811
Antonio Davide <contact@antoniodavide.dev>
2023-10-30 14:40:15 +0000

    feat(utils.h): added Overload template

--- C++/utils.h
+++ C++/utils.h
@@ -1,3 +1,9 @@
 #pragma once
 
+template<class... Ts> struct Overload : Ts... { using Ts::operator()...; };
+
+#if __cplusplus >= 202002L
+template<class... Ts> Overload(Ts...) -> Overload<Ts...>; 
+#endif // __cplusplus >= 202002L
+
 template<typename> constexpr bool always_false_v = false;
