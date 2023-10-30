639d0eb2a1936423ce3a843b8dedaf14d150d15c
Antonio Davide <contact@antoniodavide.dev>
2023-10-30 14:49:11 +0000

    feat(utils.h): added 'split_each'

--- C++/utils.h
+++ C++/utils.h
@@ -7,3 +7,18 @@ template<class... Ts> Overload(Ts...) -> Overload<Ts...>;
 #endif // __cplusplus >= 202002L
 
 template<typename> constexpr bool always_false_v = false;
+
+template<typename Function>
+void split_each(std::string_view s, char sep, Function f) {
+    size_t i = 0, start = 0;
+
+    for( ; i < s.size(); i++) {
+        if(std::isspace(s[i])) continue;
+        if(s[i] != sep) continue;
+        if(i > start) f(s.substr(start, i - start));
+        start = i + 1;
+    }
+
+    while(start < s.size() && std::isspace(s[start])) start++;
+    if(start < s.size()) f(s.substr(start));
+}
