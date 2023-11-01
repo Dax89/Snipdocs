0e080c350a0ad1c196a23fd9777639ca6ac252f4
Antonio Davide <contact@antoniodavide.dev>
2023-11-01 21:52:44 +0000

    feat(utils.cpp): added trim and splittrim_each

--- C++/utils.cpp
+++ C++/utils.cpp
@@ -17,3 +17,34 @@ std::vector<std::string_view> split(std::string_view sv, char sep)
     if(start < sv.size()) s.push_back(sv.substr(start));
     return s;
 }
+
+std::string_view trim(std::string_view v) {
+    int start = 0, end = v.size() - 1;
+
+    while(start < static_cast<int>(v.size()) && std::isblank(v[start]))
+        start++;
+
+    while(end >= 0 && std::isblank(v[end]))
+        end--;
+
+    if(start < end)
+        return v.substr(start, end - start + 1);
+
+    return std::string_view{};
+}
+
+template <typename Function>
+void splittrim_each(std::string_view s, char ch, Function f) {
+    size_t i = 0, start = 0;
+
+    for(; i < s.size(); i++) {
+        if(s[i] != ch)
+            continue;
+
+        f(trim(s.substr(start, i - start)));
+        start = i + 1;
+    }
+
+    if(start < s.size())
+        f(trim(s.substr(start)));
+}
