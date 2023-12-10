81151b9bcb8e034f1e73f0ae65770bcb95aea852
Antonio Davide <contact@antoniodavide.dev>
2023-12-10 16:26:42 +0000

    feat(C++): added pimpl idiom

--- C++/utils.h
+++ C++/utils.h
@@ -2,11 +2,17 @@
 
 #include <cctype>
 #include <string_view>
+#include <memory>
 
-template<class... Ts> struct Overload : Ts... { using Ts::operator()...; };
+template<typename T>
+using PImpl = const std::unique_ptr<T>;
+
+template<class... Ts>
+struct Overload : Ts... { using Ts::operator()...; };
 
 #if __cplusplus < 202002L
-template<class... Ts> Overload(Ts...) -> Overload<Ts...>; 
+template<class... Ts>
+Overload(Ts...) -> Overload<Ts...>; 
 #endif // __cplusplus >= 202002L
 
 template<typename> constexpr bool always_false_v = false;
