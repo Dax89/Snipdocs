3b10561542f52d3d5b75eb1c5c72f7f4cf18b14c
Antonio Davide <contact@antoniodavide.dev>
2024-01-25 20:37:42 +0000

    feat(CMake): updated FindMyModule.cmake

--- CMake/FindMyModule.cmake
+++ CMake/FindMyModule.cmake
@@ -1,30 +1,25 @@
-#find_package(PkgConfig QUIET)
-#pkg_check_modules(PC_mymodule QUIET mymodule)
+find_package(PkgConfig)
 
-if(NOT mymodule_FOUND)
-    find_path(mymodule_INCLUDE_DIR
-        NAMES mymodule.h
-        #HINTS ${PC_mymodule_INCLUDE_DIRS}
-    )
+if(PKG_CONFIG_FOUND)
+    pkg_check_modules(PC_mymodule QUIET mymodule)
+endif()
 
-    find_library(mymodule_LIBRARIES
-        NAMES mymodule
-        #HINTS ${PC_mymodule_LIBRARY_DIRS}
-    )
+if(PC_mymodule_FOUND)
+    message(STATUS "Found mymodule pkg-config\n\tPC_mymodule_INCLUDE_DIR: ${PC_mymodule_INCLUDE_DIRS}\n\tPC_mymodule_LIBRARIES: ${PC_mymodule_LIBRARIES}")
+    set(mymodule_FOUND "${PC_mymodule_FOUND}")
+    set(mymodule_INCLUDE_DIR "${PC_mymodule_INCLUDE_DIRS}")
+    set(mymodule_LIBRARIES "${PC_mymodule_LIBRARIES}")
+else()
+    find_path(mymodule_INCLUDE_DIR NAMES mymodule.h)
+    find_library(mymodule_LIBRARIES NAMES ssh2 mymodule)
 
     if(mymodule_INCLUDE_DIR AND mymodule_LIBRARIES)
         set(mymodule_FOUND TRUE)
     endif()
 
     if(mymodule_FOUND)
-        if (NOT mymodule_FIND_QUIETLY)
+        if(NOT mymodule_FIND_QUIETLY)
             message(STATUS "Found mymodule\n\tmymodule_INCLUDE_DIR: ${mymodule_INCLUDE_DIR}\n\tmymodule_LIBRARIES: ${mymodule_LIBRARIES}")
-        endif()
-
-        if(NOT TARGET mymodule)
-            add_library(mymodule INTERFACE IMPORTED GLOBAL)
-            target_include_directories(mymodule INTERFACE ${mymodule_INCLUDE_DIR})
-            target_link_libraries(mymodule INTERFACE ${mymodule_LIBRARIES})
         endif()
     elseif(mymodule_FIND_REQUIRED)
         message(FATAL_ERROR "Could not find mymodule")
@@ -35,4 +30,11 @@ if(NOT mymodule_FOUND)
         mymodule_INCLUDE_DIR
         mymodule_LIBRARIES
     )
+endif()
+
+if(NOT TARGET mymodule)
+    add_library(mymodule INTERFACE IMPORTED GLOBAL)
+    target_include_directories(mymodule INTERFACE ${mymodule_INCLUDE_DIR})
+    target_link_libraries(mymodule INTERFACE ${mymodule_LIBRARIES})
+    # add_library(mymodule::mymodule ALIAS mymodule)
 endif()
